02e2f61ae77ac6b63d316ebaaa35505d78c131da
Saravanan <saravanan@way2cbe.com>
2013-06-11 22:50:28 +0530

    Adding PHP Mailer for mailing functionality

---
+++ config/mail_config.php
@@ -0,0 +1,8 @@
+<?php
+//E-Mail configurations
+$smtp_hostname="";	//Hostname of SMTP server
+$smtp_port=25;		//SMTP Host port number
+$smtp_username="";	//SMTP Username (mail address)
+$smtp_password="";	//SMTP Password
+$smtp_frommail="";	//Will be displayed as from address
+$smtp_fromname="";	//Will be displayed as from address
