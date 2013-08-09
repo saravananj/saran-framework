8af14f2b9d2f3d82bcfe55d95b3aa4f55d093d1a
Saravanan <saravanan@way2cbe.com>
2013-08-09 23:11:08 +0530

    Adding Licence and Changing bootstrap to version 3

--- header.php
+++ header.php
@@ -10,15 +10,15 @@
 
 <!-- Inclusion of CSS and JS files of Libraries -->
 
-<!-- Bootstrap by Twitter -->
-<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
-<link rel="stylesheet" type="text/css" href="css/bootstrap-responsive.css">
-<script type="text/javascript" src="js/bootstrap.js"></script>
-
 <!-- jQuery with jQuery UI -->
 <link rel="stylesheet" type="text/css" href="css/jquery-ui-1.10.3.custom.css">
 <script type="text/javascript" src="js/jquery-1.9.1.js"></script>
 <script type="text/javascript" src="js/jquery-ui-1.10.3.custom.js"></script>
+
+<!-- Bootstrap by Twitter -->
+<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="css/bootstrap-responsive.css">
+<script type="text/javascript" src="js/bootstrap.js"></script>
 
 <!-- jQuery Datatables -->
 <link rel="stylesheet" type="text/css" href="css/jquery.dataTables.css">
--- index.php
+++ index.php
@@ -1,7 +1,37 @@
 <?php
+/**
+ * Saran Framework
+ *
+ * An open source application development framework, that has common components needed to build a static/dynamic website.
+ *
+ * NOTICE OF LICENSE
+ *
+ * Licensed under The MIT License (MIT)
+ *
+ * This source file is subject to The MIT License (MIT) that is
+ * bundled with this package in the files license.txt / license.rst.  It is
+ * also available through the world wide web at this URL:
+ * http://opensource.org/licenses/MIT
+ * If you did not receive a copy of the license and are unable to obtain it
+ * through the world wide web, please send an email to
+ * saravanan@way2cbe.com so we can send you a copy immediately.
+ *
+ * @package		Saran Framework
+ * @author		Saravanan Jayabalan
+ * @copyright	Copyright (c) 2013, Saravanan Jayabalan. (http://saravanan.way2cbe.com/)
+ * @license		http://opensource.org/licenses/MIT The MIT License (MIT)
+ * @link		https://github.com/saravananj/saran-framework
+ * @since		Version 1.0
+ */
 require_once("header.php");
 ?>
-Hi, This is a Sample home page
+<div class="container">
+	<div class="row">
+    	<div class="col-lg-12">
+			Hi, This is a Sample home page
+        </div>
+    </div>
+</div>
 <?php
 require_once("footer.php");
 ?>
